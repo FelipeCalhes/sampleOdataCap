f733d3ef83e996796fadcb3fe6e94b721ebc9777
FelipeCalhesNumen <felipe.calhes@numenit.com>
2024-11-12 16:16:30 -0300

    pack

--- srv/service.js
+++ srv/service.js
@@ -5,7 +5,7 @@ module.exports = function( ){
         const con = await cds.connect.to('metadata') // no package.json está declarado este cara
         const{Categories} = con.entities
         let ret = await con.run(SELECT('*').from(Categories)) // isso é um GET
-        let ret2 = await con.run(INSERT.into(Categories).entries({})) // isso é um POST
+        //let ret2 = await con.run(INSERT.into(Categories).entries({})) // isso é um POST
         return ret
     })
 }
